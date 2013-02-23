4e9bc199e096f0d31b949c0f398fdd46c6fba7c8
James Dennes <jdennes@gmail.com>
2013-02-23 17:33:53 +0100

    Go.

---
+++ DotNetCreateSendOAuthTest/Controllers/DefaultController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using createsend_dotnet;
+using System.Configuration;
+
+namespace DotNetCreateSendOAuthTest.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public RedirectResult Index()
+        {
+            var authorizeUrl = General.AuthorizeUrl(
+                int.Parse(ConfigurationManager.AppSettings.Get("CREATESEND_CLIENT_ID")),
+                ConfigurationManager.AppSettings.Get("CREATESEND_CLIENT_SECRET"),
+                ConfigurationManager.AppSettings.Get("CREATESEND_REDIRECT_URI"),
+                "ViewReports,CreateCampaigns,SendCampaigns");
+            return Redirect(authorizeUrl);
+        }
+
+        public ContentResult ExchangeToken()
+        {
+            var code = Request.Params.Get("code");
+            var tokenDetails = General.ExchangeToken(
+                int.Parse(ConfigurationManager.AppSettings.Get("CREATESEND_CLIENT_ID")),
+                ConfigurationManager.AppSettings.Get("CREATESEND_CLIENT_SECRET"),
+                ConfigurationManager.AppSettings.Get("CREATESEND_REDIRECT_URI"),
+                code);
+
+            var accessToken = tokenDetails.access_token;
+            var refreshToken = tokenDetails.refresh_token;
+
+            var response = "";
+            response = "<pre>";
+            response += "Your user is successfully authenticated. Here are the details you need:<br/><br/>";
+            response += "access token: " + accessToken + "<br/>";
+            response += "refresh token: " + refreshToken + "<br/>";
+            response += "<br/><br/>";
+ 
+            AuthenticationDetails auth = new OAuthAuthenticationDetails(
+                accessToken, refreshToken);
+            General general = new General(auth);
+            var clients = general.Clients();
+ 
+            response += "We've made an API call too. Here are your clients:<br/><br/>";
+            foreach (BasicClient c in clients) {
+                response += c.Name + "<br/>";
+            }
+            response += "</pre>";
+            return Content(response);
+        }
+    }
+}
---
+++ DotNetCreateSendOAuthTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DotNetCreateSendOAuthTest.MvcApplication" Language="C#" %>
